e51ae26842eeab35262745ddb31de1088b3beb37
Gourav Banerjee <gouravbanerjee@Gouravs-MacBook-Pro.local>
2026-05-04 21:22:29 +0530

    Fix Playwright dependency installation error

--- build.sh
+++ build.sh
@@ -8,9 +8,9 @@ pip install --upgrade pip
 # Install Python dependencies
 pip install -r requirements.txt
 
+# Note: Render uses Ubuntu underneath. We need to install poppler for pdf2image.
+apt-get update && apt-get install -y poppler-utils
+
 # Install Playwright browser binaries and OS dependencies
 playwright install chromium
 playwright install-deps chromium
-
-# Note: Render uses Ubuntu underneath. We need to install poppler for pdf2image.
-apt-get update && apt-get install -y poppler-utils
