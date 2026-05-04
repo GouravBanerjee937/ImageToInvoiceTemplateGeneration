3a65601f308b84e234e5558bf418bfd8b2c76bd2
Gourav Banerjee <gouravbanerjee@Gouravs-MacBook-Pro.local>
2026-05-04 20:52:33 +0530

    Add Render deployment configuration

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Install Playwright browser binaries and OS dependencies
+playwright install chromium
+playwright install-deps chromium
+
+# Note: Render uses Ubuntu underneath. We need to install poppler for pdf2image.
+apt-get update && apt-get install -y poppler-utils
